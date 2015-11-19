23c11f7467988d7330da6c735a07cdb92ef36e5b
truetamtam <truetamtam@yandex.ru>
2015-11-19 12:43:57 +0300

    awk and tmp file.

--- wbsdi.sh
+++ wbsdi.sh
@@ -42,11 +42,12 @@ awk '/return array\(/ { print; print "    \x27shop\x27 => true,"; next }1' ${WBS
 sed 's/admin/wbs/' ${WBSHOST}/wa-config/config.php.example > ${WBSHOST}/wa-config/config.php
 # db
 # note: database should already be created
-cp ${WBSHOST}/wa-config/db.php.example ${WBSHOST}/wa-config/db.php
-sed -i 's/\x27user\x27 => \x27root\x27/\x27user\x27 => \x27homestead\x27/' ${WBSHOST}/wa-config/db.php
-sed -i 's/\x27password\x27 => \x27\x27/\x27password\x27 => \x27secret\x27/' ${WBSHOST}/wa-config/db.php
-sed -i 's/\x27database\x27 =>  \x27wa\x27/\x27database\x27 => \x27'"$WBSHOSTDB"'\x27/' ${WBSHOST}/wa-config/db.php
-awk $'/\'default\' => array\(/ { print; print "                \x27sql_mode\x27 => \x27TRADITIONAL\x27,"; next }1' ${WBSHOST}/wa-config/db.php > ${WBSHOST}/wa-config/db.php
+cp ${WBSHOST}/wa-config/db.php.example ${WBSHOST}/wa-config/db.tmp.php
+sed -i 's/\x27user\x27 => \x27root\x27/\x27user\x27 => \x27homestead\x27/' ${WBSHOST}/wa-config/db.tmp.php
+sed -i 's/\x27password\x27 => \x27\x27/\x27password\x27 => \x27secret\x27/' ${WBSHOST}/wa-config/db.tmp.php
+sed -i 's/\x27database\x27 =>  \x27wa\x27/\x27database\x27 => \x27'"$WBSHOSTDB"'\x27/' ${WBSHOST}/wa-config/db.tmp.php
+awk $'/\'default\' => array\(/ { print; print "\t\t\x27sql_mode\x27 => \x27TRADITIONAL\x27,"; next }1' ${WBSHOST}/wa-config/db.tmp.php > ${WBSHOST}/wa-config/db.php
+rm -f ${WBSHOST}/wa-config/db.tmp.php
 # locale
 cp ${WBSHOST}/wa-config/locale.php.example ${WBSHOST}/wa-config/locale.php
 # system config
